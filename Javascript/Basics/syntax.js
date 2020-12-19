02d16ecdcdffa626e65df1eb068f1da68bcf3ccc
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-19 23:31:33 +0000

    Add JS syntax

--- Javascript/Basics/syntax.js
+++ Javascript/Basics/syntax.js
@@ -28,3 +28,49 @@ let number = 100;
 number = 'Ted';
 
 number = 1.232;
+
+// Strings are indexed
+
+let pokemon = 'Charizard';
+
+console.log(pokemon[1]);
+
+
+// Space is a character
+
+let whitespace = "                    ";
+
+console.log(whitespace.length);
+
+// Methods
+
+// String
+
+let randomString = Math.random().toString(20).substr(2, 14);
+
+console.log("original: " + randomString);
+
+console.log("toUpperCase: " + randomString.toUpperCase());
+
+console.log("toLowerCase: " + randomString.toLowerCase());
+
+randomString = '      ' + randomString + '        ';
+
+console.log("spaced original: " + randomString);
+
+console.log("trimmed: " + randomString.trim());
+
+let newRandomString = Math.random().toString(20).substr(2, 14) + 'tiddle';
+
+console.log("indexOf: " + newRandomString.indexOf('tiddle'));
+
+console.log("sliceOneArg: " + newRandomString.slice(4));
+
+console.log("sliceTwoArg: " + newRandomString.slice(2,8));
+
+console.log("replace: " + newRandomString.replace('tiddle', 'martin'));
+
+// Method chaining
+
+console.log("trimmed: " + randomString.trim().toUpperCase());
+
