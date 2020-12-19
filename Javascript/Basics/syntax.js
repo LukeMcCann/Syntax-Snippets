15457397f2794a7b4353e4c0add2f893e7d03855
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-19 23:03:17 +0000

    Add JS syntax snippets

---
+++ Javascript/Basics/syntax.js
@@ -0,0 +1,30 @@
+// Primitive types
+
+NaN
+true, false
+1,2,3
+0.1,1.2,1.344
+undefined 
+
+// Variable Declaration
+
+let name = 'Luke';
+
+// Global Scope
+
+var name = 'Tom'
+
+// Constants
+
+const constantName = 'Martin';
+
+
+// Javascript Variables can change type
+
+// TypeScript promotes stricter JS rules
+
+let number = 100;
+
+number = 'Ted';
+
+number = 1.232;
