d1850fa9bde634a1cf68545ca6c0bcc132c87483
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-20 21:16:47 +0000

    Add while loop examples

---
+++ Javascript/Basics/loop.js
@@ -0,0 +1,15 @@
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+const animals = ['lions', 'tigers', 'bears'];
+
+for (let i = 0; i < animals.length; i++) {
+    console.log(i, animals[i]);
+}
+
+let x = 0;
+while (x !== 20) {
+    console.log(x);
+    ++x;
+}
