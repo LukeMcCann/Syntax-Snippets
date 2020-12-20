ada558c2a38eea324e2df5765ba54159c2777775
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-20 20:07:21 +0000

    Array methods JS

--- Javascript/Basics/arrays.js
+++ Javascript/Basics/arrays.js
@@ -10,3 +10,62 @@ console.log(
         console.log(response);
     })
 );
+
+
+colors[1] = 'purple';
+
+colors[colors.length] = 'orange';
+
+colors.push('grey');
+
+colors.pop();
+
+colors.unshift('aqua');
+
+colors.forEach(function (color) {
+    console.log(color);
+});
+
+console.log(colors.shift());
+
+let cats = ['bengal', 'siamese'];
+
+let dogs = ['retriever', 'dalmation', 'beagle'];
+
+let animals = cats.concat(dogs);
+
+let humanoids = ['Homosapiens'];
+
+let beings = animals.concat(humanoids);
+
+animals.forEach(function (animal) {
+    console.log(animal);
+});
+
+beings.forEach(function (being) {
+    console.log(being);
+})
+
+console.log(beings.includes('retriever'));
+
+console.log(beings.includes('Homosapiens', 4));
+
+console.log(beings.indexOf('bengal'));
+
+console.log(beings.indexOf('bengal', 3));
+
+let numbers = [1, 4, 5, 6, 7, 8, 9, 2, 3];
+
+console.log(numbers.reverse().sort());
+
+console.log(numbers.join());
+
+console.log(typeof numbers.join());
+
+console.log(numbers.slice(0, 3));
+
+numbers.splice(2, 0, 'undefined')
+console.log(numbers);
+
+numbers.splice(2, 3);
+console.log(numbers);
