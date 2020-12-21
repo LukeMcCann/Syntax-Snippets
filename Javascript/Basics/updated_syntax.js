b6bc3537e0ab23653af3a6ec0475eb47559cdc2e
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-21 22:26:19 +0000

    Add REST example for combining parameters

--- Javascript/Basics/updated_syntax.js
+++ Javascript/Basics/updated_syntax.js
@@ -57,3 +57,45 @@ const catDog = { ...feline, ...canine }
 // { legs: 4, family: "Caninae", furry: true }
 
 // conflicting properties overwrite one another
+
+
+// REST
+// Collects items into a single array
+// the old method of this was done using arguments as below:
+
+function args() {
+    console.log(arguments);
+}
+args();
+
+// Shortocmings of arguments:
+//          arguments is not a real array unless converted using spread [...arguments]
+//          there is no functionality for collecting remainnig arguments
+//          the arguments function does not exist in arrow functions
+// arguments is a collection of the arguments passed in to the function
+// instead of iterating over these arguments in the traditional
+// method, using a for loop, we can use REST
+
+// We use REST to convert the singular numbers into an array,
+// allowing for the use of reduce on the nums array
+function sumAll(...nums) {
+    return nums.reduce((total, index) => {
+        return total + index;
+    }
+)}
+
+console.log(sumAll(100, 20, 30, 90));
+
+
+// REST may also be used to collect
+// all remaining arguments, 
+// those arguments which have not been
+// matched with a parameter
+
+function fullName(first, last, ...titles) {
+    console.log('First: ', first);
+    console.log('Last: ', last);
+    console.log('Titles: ', titles);
+}
+
+fullName('Luke', 'McCann', 'Doctor', 'Master');
