229dffec499f3067961638e60950a0a54d4befab
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-21 22:55:52 +0000

    destructuring

--- Javascript/Basics/destructuring.js
+++ Javascript/Basics/destructuring.js
@@ -24,12 +24,32 @@ console.log(bronze);
 
 console.log(raceResults);
 
+// skipping entries
 const [first, , ,fourth] = raceResults;
 
 console.log(fourth);
 
 
+// using REST operator
 const [winner, ...losers] = raceResults;
 
 console.log(winner);
 console.log(losers);
+
+
+// Destructuring objects
+
+const runner = {
+    first: "Eliud",
+    last: "Kipchoge",
+    country: "Kenya", 
+    title: "Elder of the Order of the Golden Heart of Kenya"
+}
+const {first, last, country} = runner; 
+
+first;
+last;
+country;
+
+
+ 
