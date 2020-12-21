8e6fa386cf842997d70d7a29d4d57f9c0e7b436b
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-21 19:50:08 +0000

    Add comment explanation

--- Javascript/Basics/sort.js
+++ Javascript/Basics/sort.js
@@ -5,6 +5,29 @@ const sortedAsString = prices.sort();
 
 console.log(sortedAsString);
 
+// Using compareFunc(a,b) if the compareFunc returns a negative value
+// the values are sorted a before b
+// otherwise if the compareFunc returns a positive value it sorts b before a
+// take this array for instance:
+//
+//      const prices = [400.50, 3000, 99.99, 35.99, 12.00, 9500];
+//
+//      const sortedPricesAsc = prices.sort(a, b) => a - b);
+//  
+// if we run our comapre func in the first instance we have:
+//
+//      a = 400.50;                     b = 3000;
+// 
+// in our comapreFunc we have: a - b
+// 
+// this equates to: 400.50 - 3000 = -2,599.5
+// therefore a is sorted to be before b
+//
+// coming to the end of the array we have a = 35.99 and b = 12.00
+// in this instance we have: 35.99 - 12.00 = 23.99
+//
+// 23.99 is a positive number, therefore b is ordered before a
+
 // Sort ascending
 const sortedAsInteger = prices.sort((a, b) => a - b);
 console.log(sortedAsInteger);
