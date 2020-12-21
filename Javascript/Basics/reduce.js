7b22013df40375edd480acd95f73be0997f7b404
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-21 20:18:00 +0000

    Add reducer example

---
+++ Javascript/Basics/reduce.js
@@ -0,0 +1,26 @@
+// Reduce
+// executes a provided reducer funciton on each element of an array
+// producing a single output value
+// In layments terms, it takes an array of vlaues
+// and reduces them down to a single value
+
+// Sum all elements in array
+// accumulator stores the accumulated value - the end result of reduce
+// currentValue is the current item in the array - each individual element 
+// of the array as the reducer work it's way through
+//
+// the accumulator starts as the first item in the array
+// the currentValue starts as the next element
+// in this instance this would be accumulator = 100 and currentValue = 2
+// we then add the accumulator to the currentValue = 102
+// the returned value is then used as the accumulator in the next iteration
+// with currentValue once again equating to the next value in the array
+// in this case this equates to accumulator = 102 currentValue = 5
+// this pattern continue until completion
+const unordered = [100, 2, 5, 8, 3, 10];
+
+let sum = unordered.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+});
+
+console.log(sum);
