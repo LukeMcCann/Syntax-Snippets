ff658dce90c626a88f11041663484161c430efe5
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-27 23:05:18 +0000

    Add examplesfor querySelector

--- Javascript/sandbox/app.js
+++ Javascript/sandbox/app.js
@@ -2,9 +2,13 @@
 
 
 
-// Multiple Element Selectors 
+/** 
+ * Multiple Element Selectors
+ **/
 
-// Single Element Selectors
+/** 
+ * Single Element Selectors
+ **/
 
 let task_title = document.getElementById('task-title');
 console.log(task_title);
@@ -19,3 +23,13 @@ task_title.style.padding = '5px';
 task_title.textContent = 'Hacked Content!';
 // task_title.innerText = 'Inner Text';
 task_title.innerHTML = '<h3 style="color: red; background-color: black">Task List</h3>';
+
+// Query Selector: newer thant getElementById and more flexible
+
+// Get by id
+task_title = document.querySelector('#task-title');
+console.log(task_title);
+
+// Get by class
+let card_title = document.querySelector('.card-title');
+console.log(card_title);
