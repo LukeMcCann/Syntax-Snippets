c87ab84827c528fdc72ae8a013faef7845cca625
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-27 22:01:52 +0000

    Add document selection examples

--- Javascript/sandbox/dom.js
+++ Javascript/sandbox/dom.js
@@ -7,8 +7,32 @@ console.log(document.head);
 console.log('Body:');
 console.log(document.body);
 
+console.log('Doctype:');
+console.log(document.doctype);
+
+console.log('Domain:');
+console.log(document.domain);
+
+console.log('URL:');
+console.log(document.URL);
+
+console.log('Charset:');
+console.log(document.characterSet);
+
+console.log('Content Type:');
+console.log(document.contentType);
+
+// Selecting without selectors
+// This approach is not recommended
+console.log('Forms:');
+console.log(document.forms);
+
+console.log('Specific Form:');
+console.log(document.forms[0]);
+
 // HTML collections are like arrays, but do not support foreach()
 console.log('HTML Collection:');
 console.log(document.all);
 
 console.log(document.all[2]);
+console.log(document.all[4]);
