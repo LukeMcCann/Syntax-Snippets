85283f7f282bb2a7f2ef6887d80ed0cb6afc3b35
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-27 21:44:37 +0000

    Add DOM examples

---
+++ Javascript/sandbox/dom.js
@@ -0,0 +1,14 @@
+console.log('Document:');
+console.log(document);
+
+console.log('Head:');
+console.log(document.head);
+
+console.log('Body:');
+console.log(document.body);
+
+// HTML collections are like arrays, but do not support foreach()
+console.log('HTML Collection:');
+console.log(document.all);
+
+console.log(document.all[2]);
