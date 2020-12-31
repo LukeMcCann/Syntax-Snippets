5914f57018596750208b5207a89c8cb9906c530b
Luke McCann <developer-lukemccann@outlook.com>
2020-12-31 14:46:47 +0000

    Add link targeting

--- Javascript/sandbox/app.js
+++ Javascript/sandbox/app.js
@@ -2,28 +2,57 @@
 // This means when an event is fired on a child element, the event bubbles up through
 // the parent classes
 
-document.querySelector('.card-title').addEventListener('click', (e) => {
-    console.log('card title');
-});
+// document.querySelector('.card-title').addEventListener('click', (e) => {
+//     console.log('card title');
+// });
 
-// Parent of card-title
-document.querySelector('.card-title').addEventListener('click', (e) => {
-    console.log('card title');
-});
+// // Parent of card-title
+// document.querySelector('.card-title').addEventListener('click', (e) => {
+//     console.log('card title');
+// });
 
 
-// // Parent of card-content
-document.querySelector('.card').addEventListener('click', (e) => {
-    console.log('card');
-});
+// // // Parent of card-content
+// document.querySelector('.card').addEventListener('click', (e) => {
+//     console.log('card');
+// });
 
-// // Parent of main
-document.querySelector('.col').addEventListener('click', (e) => {
-    console.log('col');
-});
+// // // Parent of main
+// document.querySelector('.col').addEventListener('click', (e) => {
+//     console.log('col');
+// });
 
 // Due to event bubbling when you click the card-title, the event bubbles up through each of
 // the parents, this in turn firest the events of each of the click functions on these elements.
 
 
 // Event Delegation
+
+// Event delegation can be thought of as the opposite of event bubbling. Rather than a child
+// firing an event and bubbling up through the DOM, event delegation is when the parent
+// fires an events and delegates it downward through the DOM to it's child element.
+
+// This can be useful for having events fire, such as a delete event in a list
+// where each list item has a delete button (like in the ul in todo.html).
+
+// Without Delegation
+// Uncomment the below lines and then try clicing the delete buttons on the list items,
+// what happens?
+
+// const delItem = document.querySelector('.delete-item');
+
+// delItem.addEventListener('click', (e) => {
+//     console.log('delete item');
+// });
+
+// only the first items button in the list will fire, this is why we need to use delegation.
+// Another instance for the use of delegation would be if there is a dynamic item
+// which was not there when the page was loaded.
+
+document.body.addEventListener('click', delItem);
+
+function delItem(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        console.log('delete item');
+    }
+}
