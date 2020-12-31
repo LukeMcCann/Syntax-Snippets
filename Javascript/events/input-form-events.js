e82040ee72f6ace7e05aed906584380478590e2b
Luke McCann <developer-lukemccann@outlook.com>
2020-12-31 13:25:21 +0000

    Add event bubbling examples

---
+++ Javascript/events/input-form-events.js
@@ -0,0 +1,61 @@
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5');
+
+// Submit
+// form.addEventListener('submit', runEvent);
+
+// Anonymous arrow function submit
+// form.addEventListener('submit', (e) => {
+//     console.log(`Event Type: ${e.type}`);
+// });
+
+// Input Events: Keydown
+// taskInput.addEventListener('keydown', runInputEvent);
+
+// Keyup
+// taskInput.addEventListener('keyup', runEvent);
+
+// Keypress
+// taskInput.addEventListener('keypress', runEvent);
+
+// Focus
+// taskInput.addEventListener('focus', runEvent);
+
+// Blur
+// taskInput.addEventListener('blur', runEvent);
+
+// Cut
+// taskInput.addEventListener('cut', runEvent);
+
+// Paste 
+// taskInput.addEventListener('paste', runEvent);
+
+// Input: fire on any kind of input event
+// taskInput.addEventListener('input', runEvent);
+
+// Changed: Used on SelectLists
+// selectList.addEventListener('change', (e) => {
+//     // do function stuff
+// });
+
+// Event Handlers
+// function runEvent(e) {
+//     alert(`Event Type: ${e.type}`);
+// }
+
+// function runEvent(e) {
+//     console.log(`Event Type: ${e.type}`);
+//     // Prevent Redirects
+//     e.preventDefault();
+// }
+
+// function runInputEvent(e) {
+//     console.log(`Event Type: ${e.type}`);
+
+        // get value of event item
+//     heading.innerText = e.target.value;
+
+//     // Prevent Redirects
+//     e.preventDefault();
+// }
