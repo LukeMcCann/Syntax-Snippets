8291aea3ee95ac990a67152617871e5746ed29bd
LukeMcCann <developer-lukemccann@outlook.com>
2021-01-11 23:26:39 +0000

    Add radix example

--- Javascript/Methods/math.js
+++ Javascript/Methods/math.js
@@ -3,3 +3,15 @@ function randomWholeNum() {
     return Math.floor(Math.random() * 20);
 }  
 console.log(randomWholeNum());
+
+// Random Range
+function randomRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Convert Binary
+// The second argument of parseInt is the radis (the base numeber)
+function convertBinToInteger(bin) {
+    return parseInt(bin, 2);
+}
+console.log(convertBinToInteger("10011"));
