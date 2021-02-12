229841825ebb5f18bf33432a5b0624b16bcf0b25
Luke McCann <Developer-lukemccann@outlook.com>
2021-02-12 20:20:23 +0000

    Manipulating Attributes

---
+++ Javascript/AttributeManipulation/js/app.js
@@ -0,0 +1,17 @@
+let h1 = document.querySelector('h1');
+let input = document.getElementsByTagName("input")[0];
+
+function hide() {
+    if (input.checked) {
+        // h1.classList.add('hidden');
+        h1.innerHTML = "<h3>Hello</h3>";
+    } else {
+        h1.textContent = "<h3>Hello</h3>";
+        // h1.classList.remove('hidden');
+    }
+}
+
+let a = document.querySelector('a');
+
+a.setAttribute("href", "https://www.ddr-digital.com");
+console.log(a.getAttribute("href"));
