c6ec469fff1f6f7a236056b273fea4e02b341357
Luke McCann <Developer-lukemccann@outlook.com>
2021-02-12 08:48:30 +0000

    Create match_expression.php

---
+++ PHP/PHP8/match_expression.php
@@ -0,0 +1,31 @@
+<?php
+
+class Conversation
+{
+}
+
+$obj = new Conversation();
+
+// Traditionally
+// switch (get_class($obj)) {
+//     case 'Conversation':
+//         $type = 'started_conversation';
+//         break;
+
+//     case 'Reply':
+//         $type = 'replied_to_conversation';
+//         break;
+
+//     case 'Comment':
+//         $type = 'commented_on_conversation'; 
+//         break;
+// }
+
+// PHP8
+$type = match (get_class($obj)) {
+    'Conversation' => 'started_conversation',
+    'Reply' => 'replied_to_conversation',
+    'Comment' => 'commented_on_conversation',
+};
+
+echo $type;
