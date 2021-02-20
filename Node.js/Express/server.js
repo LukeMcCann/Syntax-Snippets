62ca6ce3a443abbcb26bb22d0043dd8a199bfb14
LukeMcCann <developer-lukemccann@outlook.com>
2021-02-20 22:49:34 +0000

    Add contact example

--- Node.js/Express/server.js
+++ Node.js/Express/server.js
@@ -7,7 +7,11 @@ const port = '3001';
 
 app.get('/', function (request, response) {
     response.send('<h1>Hello</h1>');
-}) // Express method to specify the action to take when a request is received
+}); // Express method to specify the action to take when a request is received
+
+app.get("/contact", function (request, response) {
+    response.send('<p>Contact me</p>');
+});
 
 app.listen(port, function () {
     console.log(`Server started on port ${port}`);
