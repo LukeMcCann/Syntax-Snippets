29c4e1fb1d5c094984a7b1dd5786401f35a7b510
Luke McCann <Developer-lukemccann@outlook.com>
2021-04-19 21:51:01 +0100

    Create RGBToHex.js

---
+++ Javascript/RGBToHex.js
@@ -0,0 +1,28 @@
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+alert(rgbToHex(0, 51, 255)); // #0033ff
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+alert(hexToRgb("#0033ff").g); // "51";
+
+
+function rgbToHex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+alert(rgbToHex(0, 51, 255)); // #0033ff
