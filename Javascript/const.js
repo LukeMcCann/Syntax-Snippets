1f17ab9b979062b3d5df7364fb60b37d01827f29
Luke McCann <Developer-lukemccann@outlook.com>
2021-07-02 09:31:27 +0100

    Create const.js

---
+++ Javascript/const.js
@@ -0,0 +1,12 @@
+// In JS You can add new vlaues to a constant
+// but you cannot reassign
+
+const testConstant2 = {};
+
+testConstant2.x = 'Hello';
+testConstant2.y = 'World';
+testConstant2.x = 'Goodbye';
+testConstant2;
+
+
+// testConstant = {}; // Error
