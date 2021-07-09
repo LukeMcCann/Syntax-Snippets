461a0b1027901af5e1f1812bf38b51f86802129f
Luke McCann <Developer-lukemccann@outlook.com>
2021-07-09 11:16:49 +0100

    Create handlingHttp.js

---
+++ Node.js/API/handlingHttp.js
@@ -0,0 +1,33 @@
+'use strict';
+
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+const courses = [
+    { id: 1, name: 'Software Engineering' },
+    { id: 2, name: 'Computer Science' },
+    { id: 3, name: 'Robotics' },
+    { id: 4, name: 'Artificial intelligence' },
+    { id: 5, name: 'Machine Learning' },
+    { id: 6, name: 'Theoretical Physics'}
+];
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    res.send(course);
+});
+
+app.listen(port, (req, res) => {
+    console.log(`Listening on port ${port}`);
+});
