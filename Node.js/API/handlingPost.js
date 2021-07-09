be516d81e44875812424560bb62525fc1df6c978
Luke McCann <Developer-lukemccann@outlook.com>
2021-07-09 11:54:10 +0100

    Create handlingPost.js

---
+++ Node.js/API/handlingPost.js
@@ -0,0 +1,48 @@
+'use strict';
+
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+const courses = [
+    { id: 1, name: 'Software Engineering' },
+    { id: 2, name: 'Computer Science' },
+    { id: 3, name: 'Robotics' },
+    { id: 4, name: 'Artificial intelligence' },
+    { id: 5, name: 'Machine Learning' },
+    { id: 6, name: 'Theoretical Physics'}
+];
+
+const express = require('express');
+const app = express();
+
+// enable parsing of JSON objects in body of request
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    !course ? res.status(404).send('404 Not Found.') : res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    if (req.body.name && req.body.name.length > 3) {
+        const course = {
+            id: courses.length+1,
+            name: req.body.name
+        }
+        courses.push(course);
+        res.status(201).send(course)
+    }
+    res.status(400).send('400 Bad Request.');
+});
+
+app.listen(port, (req, res) => {
+    console.log(`Listening on port ${port}`);
+});
