8bf1d00a3b21b89dd2470442780c438d13d97f28
Luke McCann <Developer-lukemccann@outlook.com>
2021-07-14 19:11:59 +0100

    Create async.js

---
+++ Node/Async/async.js
@@ -0,0 +1,9 @@
+console.log('Before');
+setTimeout(() => {
+    console.log('Reading a user from database...')
+}, 2000);
+console.log('After');
+
+// Before
+// After
+// Reading a user fro database...
