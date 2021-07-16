3968c4000fdcc239d16bf956ddc5d4582cf5d81b
Luke McCann <Developer-lukemccann@outlook.com>
2021-07-16 19:22:00 +0100

    Update Parallel-Promises.js

--- Javascript/promises/Parallel-Promises.js
+++ Javascript/promises/Parallel-Promises.js
@@ -14,18 +14,23 @@ const p2 = new Promise((resolve, reject) => {
     }, 2000);
 });
 
-// const p3 = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         console.log('Async OP 3!');
-//         reject(new Error('Something went wrong!'));
-//     }, 2000);
-// });
+const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Async OP 3!');
+        reject(new Error('Something went wrong!'));
+    }, 2000);
+});
 
 Promise
     .all([p1, p2])
     .then(result => console.log(result))
-    .catch(err => console.log(err.message));
+    .catch(err => console.log('Error', err.message));
 
 // This will kick off both operations almost at the same time.
 // It is important to realise we cannot have these operations run
 // exactly at the same time as we are dealing with a single thread. 
+//
+// If we wanted to do something as soon as a single promise
+// completes we could use: Promise.race
+// As soon as the first promise is resolved, the whole
+// thing is resolved returning the result of the first promise to resolve
