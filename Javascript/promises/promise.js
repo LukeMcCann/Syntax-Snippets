fffaa2ac2ca316530e7218d650a3351eab86fbda
Luke McCann <Developer-lukemccann@outlook.com>
2021-07-16 18:58:20 +0100

    Update promise.js

--- Javascript/promises/promise.js
+++ Javascript/promises/promise.js
@@ -1,5 +1,9 @@
-const res = Promise.resolve({ id: 1 }) // returns a resolved promise
+const res = Promise.resolve({ id: 1 }); // returns a resolved promise
 res.then(result => console.log(result));
 
 const rej = Promise.reject(new Error('Promise Rejected!'));
 rej.catch(err => console.log(err));
+
+
+// When rejecting a promise it is best to create a new Error
+// as JS shows a stack trace for every Error thrown.
