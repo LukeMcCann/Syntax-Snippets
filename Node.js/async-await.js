c2610937b0b66e234042c996795d1f4c13e75f0b
Luke McCann <Developer-lukemccann@outlook.com>
2021-07-19 21:59:22 +0100

    Update async-await.js

--- Node.js/async-await.js
+++ Node.js/async-await.js
@@ -1,8 +1,17 @@
+// Promise
 const makeRequest = () =>
   getJSON()
     .then(data => {
       console.log(data)
       return "done"
     })
+
+makeRequest()
+
+// Async Await
+const makeRequest = async () => {
+  console.log(await getJSON())
+  return "done"
+}
 
 makeRequest()
