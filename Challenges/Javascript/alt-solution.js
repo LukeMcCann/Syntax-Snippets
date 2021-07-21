890fd21d5f3f2d2ee5efaeed9ff21a93764f7485
Luke McCann <Developer-lukemccann@outlook.com>
2021-07-21 20:54:55 +0100

    Create alt-solution.js

---
+++ Challenges/Javascript/alt-solution.js
@@ -0,0 +1,41 @@
+async function notifyCustomer() {
+  const customer = await getCustomer(1);
+  console.log('Customer: ', customer);
+  if (customer.isGold) {
+    const movies = await getTopMovies();
+    console.log('Top movies: ', movies);
+    await sendEmail(customer.email, movies);
+    console.log('Email sent...');
+  }
+}
+notifyCustomer();
+
+
+function getCustomer(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ 
+        id: 1, 
+        name: 'Mosh Hamedani', 
+        isGold: true, 
+        email: 'email' 
+      });
+    }, 4000);  
+  });
+}
+
+function getTopMovies() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(['movie1', 'movie2']);
+    }, 4000);
+  });
+}
+
+function sendEmail(email, movies) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 4000);
+  });
+}
