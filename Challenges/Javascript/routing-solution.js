ba35ddfd15cd1bd8fd1e6784a0a8eb089b1f66a4
Luke McCann <Developer-lukemccann@outlook.com>
2021-07-28 21:30:22 +0100

    Create routing-solution.js

---
+++ Challenges/Javascript/routing-solution.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+const port = 3000;
+const host = 'localhost';
+
+const server = require('http').createServer((req, res) => {
+
+    const url = req.url;
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    switch (url) {
+        case '/':
+            res.statusCode = 200;
+            res.end('<h1>Hello World</h1>');
+            break;
+        case '/makeABrew':
+            res.statusCode = 418;
+            res.end('<p>I\'m a teapot</p>');
+        default: 
+            res.statusCode = 404;
+            res.end('404 Not Found!');
+    }
+
+});
+
+server.listen(port, () => {
+    console.log(`Listening on http://${host}:${port}`);
+});
