a27c4f0b5cd114243865e7682c4ac48f5d6a6845
Luke McCann <Developer-lukemccann@outlook.com>
2021-08-29 12:59:29 +0100

    Create constructor.js

---
+++ Javascript/OOP/constructor.js
@@ -0,0 +1,34 @@
+// Constructor functions are like constructors of other languages.
+// JS's falvor of OOP differentiates slightly from the OOP implementation
+// of other languages like Java or PHP. 
+
+// This is due to the functioning of the Prototype chain
+// a highly powerful feature in JS. Rather than defining a 
+// Class in the traditional sense, we instead define a constructor
+// function. We then add to the objects prototype to define methods
+// for the class to use. We can therefore picture our constructor
+// as that of a constructor function in Java, without the wrapping
+// class, and treat our Object as if it were defined as a class 
+// with some additional features of the prototype chain (such as allowing
+// prototypal inheritance). 
+
+// ES6 does provide class syntax, but it is important to note that this
+// is nothing more than syntactic sugar, JS sees this and creates a 
+// constructor function and prototypal methods behind the scenes.
+
+function Color(r,g,b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+}
+
+Color.prototype.rgb = function() {
+    const {r, g, b} = this;
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+let red = new Color(255, 0, 0);
+
+console.dir(red);
+
+red.rgb();
