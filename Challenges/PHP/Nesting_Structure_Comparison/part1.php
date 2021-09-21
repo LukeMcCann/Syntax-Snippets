f5dce94f9c027c7eda7f2200e4916b834ddb8a1b
Luke McCann <Developer-lukemccann@outlook.com>
2021-09-21 09:40:33 +0100

    Update part1.php

--- Challenges/PHP/Nesting_Structure_Comparison/part1.php
+++ Challenges/PHP/Nesting_Structure_Comparison/part1.php
@@ -3,6 +3,16 @@
 // First part of the Nesting Structure Comparison, 
 // this will check basic arrays, needs to be expanded 
 // to ensure the structure is exactly equal. 
+
+// It should be remembered that 
+// we need to exit out of subarrays 
+// back to the original array, 
+// as we need to iterate to the end of 
+// both, we must therefore remember our
+// position upon entering the sub_array, 
+// and return to the point after that
+// item in the parent array to continue
+// execution.
 function same_structure_as(array $a, array $b): bool {
     $comparator_points = 0;
     $a_points = calculate_structure_points($a, $comparator_points);
