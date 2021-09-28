f834070c61a841e2c2fd71f84cc7166abe0c5b71
Luke McCann <Developer-lukemccann@outlook.com>
2021-09-28 20:21:33 +0100

    Create VariableVariables.php

---
+++ PHP/VariableVariables.php
@@ -0,0 +1,14 @@
+<?php 
+
+// PHP Variable variables
+// take the value assigned to the variable
+// and create a new variable using the value as
+// the name of the variable. 
+
+$a = 'hello';
+
+$$a = 'world';
+
+echo $a . " $hello";
+
+// prints "hello world"
