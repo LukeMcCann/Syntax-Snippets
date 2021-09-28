4559a4ebc1de327bdc6a33b2647b2e83e76dee3a
Luke McCann <Developer-lukemccann@outlook.com>
2021-09-28 21:04:39 +0100

    Create globals.php

---
+++ PHP/globals.php
@@ -0,0 +1,11 @@
+<?php 
+
+// PHP stores variables declared as global in the GLOBALS{] array
+
+$GLOBALS['x'] = 10;
+
+// this may also be declared outside a function and accessed within it using the
+// global keyword.
+
+global $x;
+
