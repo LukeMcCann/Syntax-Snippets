4e7e9c88d360153164caedc84f819e6c3cc75cc0
Luke McCann <Developer-lukemccann@outlook.com>
2021-09-28 21:11:39 +0100

    Create staticVariables.php

---
+++ PHP/staticVariables.php
@@ -0,0 +1,21 @@
+<?php 
+
+function getValue() {
+    static $value = null;
+
+    if ($value === null) {
+        $value = expensiveFunction();
+    }
+
+    return $value . "<br />";
+}
+
+function expensiveFunction() {
+    sleep(2);
+
+    return 10;
+};
+
+for ($i = 0; $i < 3; $i++) {
+    echo getValue();
+}
