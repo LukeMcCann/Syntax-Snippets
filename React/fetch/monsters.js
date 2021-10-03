30add656fea21d02dd2f95ad5b9888b5564a68ad
Luke McCann <Developer-lukemccann@outlook.com>
2021-10-03 19:06:26 +0100

    Create monsters.js

---
+++ React/fetch/monsters.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [] // Initial state defined empty
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users })); // update initial state 
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}>{ monster.name }</h1>)
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
