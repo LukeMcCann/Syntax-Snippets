8ea38e4d353a127c1ffc0f98e62b34063b2138a9
Luke McCann <Developer-lukemccann@outlook.com>
2021-10-28 19:08:39 +0100

    Create http-foundation-api.php

---
+++ Symfony/http-foundation-api.php
@@ -0,0 +1,25 @@
+
+<?php
+
+// the URI being requested (e.g. /about) minus any query parameters
+$request->getPathInfo();
+
+// retrieve GET and POST variables respectively
+$request->query->get('foo');
+$request->request->get('bar', 'default value if bar does not exist');
+
+// retrieve SERVER variables
+$request->server->get('HTTP_HOST');
+
+// retrieves an instance of UploadedFile identified by foo
+$request->files->get('foo');
+
+// retrieve a COOKIE value
+$request->cookies->get('PHPSESSID');
+
+// retrieve an HTTP request header, with normalized, lowercase keys
+$request->headers->get('host');
+$request->headers->get('content-type');
+
+$request->getMethod();    // GET, POST, PUT, DELETE, HEAD
+$request->getLanguages(); // an array of languages the client accepts
