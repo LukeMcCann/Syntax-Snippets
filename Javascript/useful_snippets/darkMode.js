66dd5bb49983b7088501b815b14b242bafb2ad8a
Luke McCann <Developer-lukemccann@outlook.com>
2021-11-26 08:23:12 +0000

    Create darkMode.js

---
+++ Javascript/useful_snippets/darkMode.js
@@ -0,0 +1,6 @@
+// I use the snippet to run on the Postgres docs and assist with my reading
+// due to my terrible eyesight, especially with bright color schemes. 
+
+document.body.style.backgroundColor = "#202124";
+document.body.style.color = "#bdc1c6";
+document.body.style.fontSize = '36px';
