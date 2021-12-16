a8e7531a26386eb1bbb37bfd4ff399e6056d2a86
Luke McCann <Developer-lukemccann@outlook.com>
2021-12-16 09:21:14 +0000

    Create add.spec.mjs

---
+++ Node-ES6/Testing/Unit/Assert/add.spec.mjs
@@ -0,0 +1,11 @@
+import { add } from './myLib.mjs';
+import assert from 'assert';
+
+console.log('add() : Should add two numbers');
+try {
+    assert.strictEqual(add(1, 2), 3);
+    console.log('SUCCESS');
+} catch(err) {
+    console.log('FAIL');
+    console.log(error(err));
+}
