0e2cc05757032512b6fc4ccbeff360bf7a1b84e1
Luke McCann <Developer-lukemccann@outlook.com>
2021-12-17 13:27:38 +0000

    Update ext.mjs
    Add dirname alternative ES6

--- cli/node/ext.mjs
+++ cli/node/ext.mjs
@@ -4,40 +4,55 @@
 
 import minimist from 'minimist';
 import fs from 'fs';
-import path from 'path';
+import path, { dirname } from 'path';
 import getStdin from 'get-stdin';
 import util from 'util';
+import { fileURLToPath } from 'url';
 
 const args = minimist(process.argv.slice(2), {
-    boolean: [ "help", "in"], // help will always be treated as boolean
-    string: [ "file", "out" ], // Anything in the file list is always considered a string
+    boolean: [ "help", "in"], 
+    string: [ "file", "out" ],
 });
 
-if (args.help) {
-    printHelp();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const BASEPATH =
+	path.resolve(process.env.BASE_PATH || __dirname);
+
+
+if (args.help || process.argv.length <= 2) {
+	error(null,/*showHelp=*/true);
+}
+else if (args._.includes("-") || args.in) {
+	getStdin().then(processFile).catch(error);
 }
 else if (args.file) {
-    fs.readFile(path.resolve(args.file), (err, content) => {
-        if (err) {
-            error(err.toString());
-        } else {
-            processFile(content.toString());
-        }
-    });
+	let filePath = path.join(BASEPATH,args.file);
+	fs.readFile(filePath,function(err,contents){
+		if (err) error(err.toString());
+		else processFile(contents.toString());
+	});
 }
 else if (args.out) {
     console.log(args.out);
 }
-else if (args.in || args._.includes("-")) {
-    getStdin()
-        .then(processFile)
-        .catch(error);
-}
 else {
-    error('Incorrect usage.', true);
+	error("Usage incorrect.",/*showHelp=*/true);
 }
 
 // **************************************************
+
+function printHelp() {
+    console.log('ex1 usage:');
+    console.log('   ex1.js --help');
+    console.log('   ex1.js --file={FILENAME}');
+    console.log('');
+    console.log('--help                  print this help');
+    console.log('--file={FILENAME}       process the file');
+    console.log('--out={INPUT}           print to standard output');
+    console.log('--in, -                 process stdin');
+    console.log('');
+}
 
 function processFile(content) {
     content = content.toUpperCase();
@@ -51,16 +66,4 @@ function error(msg, getHelp = false) {
         console.log('');
         printHelp();
     }
-}
-
-function printHelp() {
-    console.log('ex1 usage:');
-    console.log('   ex1.js --help');
-    console.log('   ex1.js --file={FILENAME}');
-    console.log('');
-    console.log('--help                  print this help');
-    console.log('--file={FILENAME}       process the file');
-    console.log('--out={INPUT}           print to standard output');
-    console.log('--in, -                 process stdin');
-    console.log('');
 }
