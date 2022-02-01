9afc0db17364ea65f3c23839fff3a46ac4607269
Luke McCann <Developer-lukemccann@outlook.com>
2022-02-01 11:30:35 +0000

    Create useContext.jsx

---
+++ React/useContext.jsx
@@ -0,0 +1,55 @@
+import { useState, createContext, useContext } from "react";
+import ReactDOM from "react-dom";
+
+const UserContext = createContext();
+
+function Component1() {
+  const [user, setUser] = useState("John Smith");
+
+  return (
+    <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}!`}</h1>
+      <Component2 user={user} />
+    </UserContext.Provider>
+  );
+}
+
+function Component2() {
+  return (
+    <>
+      <h1>Component 2</h1>
+      <Component3 />
+    </>
+  );
+}
+
+function Component3() {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <Component4 />
+    </>
+  );
+}
+
+function Component4() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <Component5 />
+    </>
+  );
+}
+
+function Component5() {
+  const user = useContext(UserContext);
+
+  return (
+    <>
+      <h1>Component 5</h1>
+      <h2>{`Hello ${user} again!`}</h2>
+    </>
+  );
+}
+
+ReactDOM.render(<Component1 />, document.getElementById("root"));
