c9420ac46b81c0bcf912b0de166b5fe919d7782e
Luke McCann <Developer-lukemccann@outlook.com>
2022-04-12 20:23:55 +0100

    Update types.ts

--- TypeScript/core/types.ts
+++ TypeScript/core/types.ts
@@ -2,8 +2,10 @@ const int = 1 as number;
 const double = 1.23 as number;
 const string = 'Hi' as string;
 const template = `Hi` as string;
+const bool = true as boolean;
 
 console.log({ int: typeof int });
 console.log({ double: typeof double });
 console.log({ string: typeof string });
 console.log({ template: typeof template });
+console.log({ boolean: typeof bool});
