9b881b06e90c996c134f20aaaa9e62bd36180540
Luke McCann <Developer-lukemccann@outlook.com>
2022-04-12 20:22:28 +0100

    Create types.ts

---
+++ TypeScript/core/types.ts
@@ -0,0 +1,9 @@
+const int = 1 as number;
+const double = 1.23 as number;
+const string = 'Hi' as string;
+const template = `Hi` as string;
+
+console.log({ int: typeof int });
+console.log({ double: typeof double });
+console.log({ string: typeof string });
+console.log({ template: typeof template });
