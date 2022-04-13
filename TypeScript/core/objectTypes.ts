24737c42d33b88522eff5e908dbea2fe24574ddc
Luke McCann <Developer-lukemccann@outlook.com>
2022-04-13 21:52:14 +0100

    Update objectTypes.ts

--- TypeScript/core/objectTypes.ts
+++ TypeScript/core/objectTypes.ts
@@ -17,4 +17,23 @@ const person: {
     nickname: 'Newt',
 };
 
+// Nested Object Type
+const product: {
+    id: string,
+    price: number,
+    tags: string[],
+    details: {
+        title: string,
+        description: string,
+    }
+} = {
+    id: 'abc1',
+    price: 12.99,
+    tags: ['great-offer', 'hot-and-new'],
+    details: {
+      title: 'Red Carpet',
+      description: 'A great carpet - almost brand-new!'
+    }
+  }
+
 console.log(person.nickname);
