e165216513d409532650ef0a5ffb93bbd265a029
Luke McCann <Developer-lukemccann@outlook.com>
2022-04-13 22:15:31 +0100

    Create arrayTypes.ts

---
+++ TypeScript/core/arrayTypes.ts
@@ -0,0 +1,11 @@
+const numbers: number[] = [1, 2, 3];
+const words: string[] = ['hello', 'moto'];
+const booleans: boolean[] = [true, false];
+const objects: object[] = [{}, {}];
+
+const arrArr: [][] = [ [], [] ];
+
+console.log('number', numbers);
+console.log('words', words);
+console.log('booleans', booleans);
+console.log('objects', objects);
