a679faf9b503bef50893d4b98469681b85be4c02
Luke McCann <Developer-lukemccann@outlook.com>
2022-04-16 12:46:46 +0100

    Create unknown.ts

---
+++ TypeScript/snippets/unknown.ts
@@ -0,0 +1,9 @@
+let vAny: any = 10;          // We can assign anything to any
+let vUnknown: unknown =  10; // We can assign anything to unknown just like any 
+
+
+let s1: string = vAny;     // Any is assignable to anything 
+let s2: string = vUnknown; // Invalid; we can't assign vUnknown to any other type (without an explicit assertion)
+
+vAny.method();     // Ok; anything goes with any
+vUnknown.method(); // Not ok; we don't know anything about this variable
