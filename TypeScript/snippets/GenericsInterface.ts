30586330fb5f77515b4db582f7ebe4e20adbb10a
Luke McCann <Developer-lukemccann@outlook.com>
2022-04-17 21:20:14 +0100

    Update GenericsInterface.ts

--- TypeScript/snippets/GenericsInterface.ts
+++ TypeScript/snippets/GenericsInterface.ts
@@ -2,7 +2,7 @@ interface Lengthy {
     length: number,
 }
 
-function countAndDescribe<T extends Lengthy> (element: T) {
+function countAndDescribe<T extends Lengthy> (element: T) : [T, string] {
     let descriptionText = 'No Value!';
     if (element.length > 0) {
         descriptionText = `The element is of length ${element.length}.`;
