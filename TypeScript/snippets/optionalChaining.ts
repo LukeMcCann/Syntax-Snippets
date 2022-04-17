02fac388d15a96426a6fb40f6ea45c2275d3fec2
Luke McCann <Developer-lukemccann@outlook.com>
2022-04-17 17:46:25 +0100

    Create optionalChaining.ts

---
+++ TypeScript/snippets/optionalChaining.ts
@@ -0,0 +1,8 @@
+const fetchedUserData = {
+    id: 1,
+    name: 'Darryl',
+    // job: { title: 'Developer', description: 'Web developer'}
+}
+
+// console.log(fetchedUserData.job && fetchedUserData.job.title);
+console.log(fetchedUserData?.job.title);
