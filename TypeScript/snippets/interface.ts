4e10072d092511d5a6bc04766dbc1f11da5f6a99
Luke McCann <Developer-lukemccann@outlook.com>
2022-04-17 16:43:33 +0100

    Update interface.ts

--- TypeScript/snippets/interface.ts
+++ TypeScript/snippets/interface.ts
@@ -2,6 +2,12 @@
 // once we have an interface created we can use it as we would a type
 // TS will enforce that our objects then adhere to the same shape as out interface.
 
+
+// whilst we can often use types and interfaces interchangeably they are not of exact equivalence.
+// We would often use an interface in conjunction with classes to enforce a contract that the 
+// class must adhere to.
+
+
 interface Person {
     name: string,
     age: number,
