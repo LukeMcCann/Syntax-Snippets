3dc5938acef7c2d3fcd89fbd7305a3665257bbfd
Luke McCann <Developer-lukemccann@outlook.com>
2022-04-27 08:29:16 +0100

    Create DiacriticAndSpecialValidation.js

---
+++ regex/DiacriticAndSpecialValidation.js
@@ -0,0 +1,4 @@
+
+export const ALPHANUMERIC_WHITESPACE_PATTERN_DIACRITIC_AND_SPECIAL = /^[Å-žA-zÀ-ÿ\d[ -@[-`¡$₠€£¿"{-~¬]*$/gi;
+
+export const ALPHANUMERIC_WITHOUT_WHITESPACE_PATTERN_DIACRITIC_AND_SPECIAL = /^[Å-žA-zÀ-ÿ\d[!-@[-`¡$₠€£¿"{-~¬]*$/gi;
