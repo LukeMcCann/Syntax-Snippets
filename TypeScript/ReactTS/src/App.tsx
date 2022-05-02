0b8cad24ec03c5cee6e63857a44b5480380c0c34
Luke McCann <Developer-lukemccann@outlook.com>
2022-05-02 10:26:25 +0100

    Add files via upload

---
+++ TypeScript/ReactTS/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+
+const App: React.FC = () => {
+  const todos = [{ id: 't1', text: 'Finish the course' }];
+  return (
+    <div className="App">
+      <NewTodo />
+      <TodoList items={todos} />
+    </div>
+  );
+};
+
+export default App;
