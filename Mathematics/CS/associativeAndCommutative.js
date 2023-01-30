d843a98df68dde543e43fcb63a4cc01206368844
Luke McCann <luke.mccann@connexone.co.uk>
2023-01-30 23:23:42 +0000

    Create associativeAndCommutative.js

---
+++ Mathematics/CS/associativeAndCommutative.js
@@ -0,0 +1,41 @@
+addition is commutative - the order of operation does not matter
+
+2 + 5 = 5 + 2
+
+addition is associative
+
+9 + 2 + 3 = 14
+2 + 3 + 9 = 14
+
+subtraction is not commutative - the order of operation does matter
+
+25 - 30 = -5
+30 - 25 = 5 
+
+subtraction is not associative
+
+10 - (5 - 1) = 6 
+(10 - 5) - 1 = 4 
+
+
+Multiplication can be viewed as repeated addition 
+
+Multiplication is commutative 
+
+2 x 5 = 5 x 2
+
+Multiplication is associative
+
+2 x 3 x 4 = 24
+6 x 4 = 24
+2 x 12 = 24
+
+Division is not associative
+
+Division is not commutative - division can be viewed as repeated subtraction
+
+(10 / 5) / 2 = 1
+10 / (5 / 2) = 4 
+
+
+
