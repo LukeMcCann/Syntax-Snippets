a5c15dbf7931b8e4eb7d3beda6e556db0175c22b
Luke McCann <luke.mccann@connexone.co.uk>
2023-09-06 20:42:18 +0100

    Update convertBinary.ts

--- convertBinary.ts
+++ convertBinary.ts
@@ -5,5 +5,29 @@ const convertBinaryToDec = (binNum: string) => {
     }, 0);
 }
 
-const decimalValue = convertBinaryToDec('010010');
-console.log(decimalValue); // Output: 18
+const convertDecToBinary = (decNum: number | string): string => {
+    let numberToConvert = Number(decNum);
+    if (isNaN(numberToConvert) || numberToConvert < 0 || !Number.isInteger(numberToConvert)) {
+        throw new Error('Input is not a valid positive integer.');
+    }
+
+    if (numberToConvert === 0) {
+        return '0';
+    }
+
+    const binaryNum = [];
+
+    while (numberToConvert > 0) {
+        const remainder = numberToConvert % 2;
+        binaryNum.push(remainder);
+        numberToConvert = Math.floor(numberToConvert / 2);
+    }
+    const binaryString = binaryNum.reverse().join('');
+    
+    return binaryString;
+}
+
+
+const binaryValue = convertDecToBinary(137);
+console.log(binaryValue); 
+
