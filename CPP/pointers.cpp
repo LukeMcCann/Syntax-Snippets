4adef317d8724c9cf24954b15ef43819664a1575
Luke McCann <luke.mccann@connexone.co.uk>
2023-09-09 19:13:07 +0100

    Update pointers.cpp

--- CPP/pointers.cpp
+++ CPP/pointers.cpp
@@ -17,12 +17,20 @@ int main() {
   cout << "myDynamicPointer: " << myDynamicPointer << endl;
   cout << "myDynamicPointer Value: " << *myDynamicPointer << endl;
 
+  cout << endl;
+
   // We must always dereference our pointers, removing them from the heap,
   // otherwise we end up with memory leaks.
 
   // Remember pointers are always simply an address, it does not really matter
   // what type we put before the pointer, we are simply declaring what type
   // we expect to be stored in that address.
+
+  *myPointer = 5; // Deference pointers and store a value in the address it points to.
+  *myDynamicPointer = 7;
+
+  cout << "myPointer Value: " << *myPointer << endl;
+  cout << "myDynamicPointer Value: " << *myDynamicPointer << endl;
 
   return 0;
 }
