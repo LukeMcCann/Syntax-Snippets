0e987a0d5e81bf8811c863ad64225d08859771fa
Luke McCann <luke.mccann@connexone.co.uk>
2023-10-26 21:00:27 +0100

    Create pointerExample.cpp

---
+++ C++/pointerExample.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct User {
+    string username;
+    string password;
+};
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <file_path>" << endl;
+        return 1;
+    }
+
+    const char* filePath = argv[1];
+
+    User u;
+    vector<User*> userVector;
+    ifstream readStream;
+
+    string username;
+    string password;
+
+    readStream.open(filePath);
+
+    while (readStream >> username >> password) {
+        User *u = new User;
+        u->username = username;
+        u->password = password;
+
+        userVector.push_back(u);
+    }
+
+    for (int i = 0; i < userVector.size(); i++) {
+        cout << "Username: " << userVector[i]->username << endl;
+        cout << "Password: " << userVector[i]->password << endl;
+    }
+
+    readStream.close();
+
+    return 0;
+}
